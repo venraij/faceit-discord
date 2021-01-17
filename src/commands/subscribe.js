26ce27da8270897649f338e0017c0efc624c78c1
Nick van Raaij <S1149241@student.windesheim.nl>
2021-01-17 14:09:54 +0100

    new class for discord channel subscriptions

---
+++ src/commands/subscribe.js
@@ -0,0 +1,67 @@
+module.exports = {
+    faceitRoom,
+    faceitHubOngoingMatches,
+    faceiteHubs,
+    faceitChampionshipOngoingMatches,
+    faceitTeamTournaments
+}
+
+/**
+ * Subscribes a specified Discord channel to a specified FACEIT chat room.
+ *
+ * @author Nick van Raaij
+ * @param {string} channel_name - A Discord channel name.
+ * @param {string} room_id - A FACEIT chatroom id.
+ * @return {Promise} Resolved promise
+ * */
+async function faceitRoom(channel_name, room_id) {
+
+}
+
+/**
+ * Subscribes a specified Discord channel to a specified FACEIT hub's matches.
+ *
+ * @author Nick van Raaij
+ * @param {string} channel_name - A Discord channel name.
+ * @param {string} hub_id - A FACEIT hub id.
+ * @return {Promise} Resolved promise
+ * */
+async function faceitHubOngoingMatches(channel_name, hub_id) {
+
+}
+
+/**
+ * Subscribes a specified Discord channel to a specified FACEIT organizers hubs.
+ *
+ * @author Nick van Raaij
+ * @param {string} channel_name - A Discord channel name.
+ * @param {string} organizer_id - A FACEIT organizer id.
+ * @return {Promise} Resolved promise
+ * */
+async function faceiteHubs(channel_name, organizer_id) {
+
+}
+
+/**
+ * Subscribes a specified Discord channel to a specified FACEIT championships active matches.
+ *
+ * @author Nick van Raaij
+ * @param {string} channel_name - A Discord channel name.
+ * @param {string} championship_id - A FACEIT championship id.
+ * @return {Promise} Resolved promise
+ * */
+async function faceitChampionshipOngoingMatches(channel_name, championship_id) {
+
+}
+
+/**
+ * Subscribes a specified Discord channel to a specified FACEIT teams tournaments.
+ *
+ * @author Nick van Raaij
+ * @param {string} channel_name - A Discord channel name.
+ * @param {string} team_id - A FACEIT team id.
+ * @return {Promise} Resolved promise
+ * */
+async function faceitTeamTournaments(channel_name, team_id) {
+
+}
